0472fb52e799cf2c73d98c7a32d3f659c3d4ac9b
Erhan Delen <Erhandln611@gmail.com>
2024-03-16 06:10:00 +0300

    java değişkenler veri tipleri

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        byte b = 5;
+        short c = 100;
+        long d = 1000000000L;
+        double e = 3.14;
+        char f = 'ؼ';
+        boolean g = true;
+        float h = 2.5f;
+        String l = "odev1";
+
+        System.out.println("intsSayi: " + a);
+        System.out.println("byteSayi: " + b);
+        System.out.println("shortSayi: " + c);
+        System.out.println("longSayi: " + d);
+        System.out.println("doubleSayi: " + e);
+        System.out.println("charKarakteri: " + f);
+        System.out.println("booleanDoğrumu: " + g);
+        System.out.println("floatSayi: " + h);
+        System.out.println("stringKarakteri: " + l);
+    }
+}
